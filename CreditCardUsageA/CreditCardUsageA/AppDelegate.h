d8be12a82ecd9a6c999e43cc5e78349ce25502a3
housenkui <housenkui@mobanker.com>
2018-01-24 16:08:26 +0800

    创建了一个single View项目

---
+++ CreditCardUsageA/CreditCardUsageA/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CreditCardUsageA
+//
+//  Created by housenkui on 2018/1/24.
+//  Copyright © 2018年 housenkui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CreditCardUsageA/CreditCardUsageA/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CreditCardUsageA
+//
+//  Created by housenkui on 2018/1/24.
+//  Copyright © 2018年 housenkui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
